053f94927144e6ef865ab2a39a181355d5f43fba
starboysa <felux47@yahoo.com>
2014-01-24 14:22:48 -0800

    Initial commit

---
+++ src/us/rjuhsd/ohs/OHSApp/InitialActivity.java
@@ -0,0 +1,15 @@
+package us.rjuhsd.ohs.OHSApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InitialActivity extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
