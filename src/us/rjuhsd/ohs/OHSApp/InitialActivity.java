f412eeec8a0cf2c08aa1a8ab129c9babfec048a6
Starboysa <felux47@yahoo.com>
2014-01-24 17:18:46 -0800

    Readding Notification class

--- src/us/rjuhsd/ohs/OHSApp/InitialActivity.java
+++ src/us/rjuhsd/ohs/OHSApp/InitialActivity.java
@@ -2,14 +2,22 @@ package us.rjuhsd.ohs.OHSApp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class InitialActivity extends Activity {
-	/**
-	 * Called when the activity is first created.
-	 */
+	//Variables:
+	private LinearLayout OHSNoteViewer;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		this.getAllByID();
+		new OHSNotification(0, "TEST", OHSNoteViewer, this);
+	}
+
+	public void getAllByID() {
+		OHSNoteViewer = (LinearLayout) this.findViewById(R.id.OHSNoteViewer);
 	}
 }
---
+++ src/us/rjuhsd/ohs/OHSApp/OHSNotification.java
@@ -0,0 +1,24 @@
+package us.rjuhsd.ohs.OHSApp;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class OHSNotification {
+	//This might have to increase in size to use all the screen real estate; however, for now it'll remain small until we decide on a layout.
+	public OHSNotification(int srcID, String text, LinearLayout l, Context c) {
+		LinearLayout ll = new LinearLayout(c);
+		ImageView iv = new ImageView(c);
+		TextView tv = new TextView(c);
+
+		ll.setOrientation(LinearLayout.HORIZONTAL);
+		//TODO: Add imageView code with srcID
+		iv.setImageResource(R.drawable.test);
+		tv.setText(text);
+
+		l.addView(ll);
+		ll.addView(iv);
+		ll.addView(tv);
+	}
+}
