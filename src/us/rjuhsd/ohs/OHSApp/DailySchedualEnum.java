a425ae29b52d5281a10f1a97617cb5f2705710b0
starboysa <felux47@yahoo.com>
2014-01-28 14:00:10 -0800

    Changing from an if else to a for loop.

--- src/us/rjuhsd/ohs/OHSApp/DailySchedualEnum.java
+++ src/us/rjuhsd/ohs/OHSApp/DailySchedualEnum.java
@@ -1,26 +1,28 @@
 package us.rjuhsd.ohs.OHSApp;
 
 public enum DailySchedualEnum {
-	INTERVENTION(new int[][]{{7, 45}, {9, 5}, {9, 40}, {11, 11}, {11, 41}, {12, 38}, {13, 8}, {12, 38}, {14, 35}});
+	INTERVENTION(new int[][]{{7, 45}, {9, 5}, {9, 40}, {11, 11}, {0, 0}, {11, 41}, {12, 38}, {13, 8}, {14, 35}});
 
 	public int[] ss;
 	public int[] e1;
 	public int[] e2;
 	public int[] inter;
-	public int[] elunch1;
-	public int[] slunch2;
-	public int[] e3l1;
-	public int[] e3l2;
+	public int[] rally;
+	public int[] lunch1;
+	public int[] lunch2;
+	public int[] e3;
 	public int[] e4;
+	public int[][] originalInput;
 	DailySchedualEnum(int[][] args) {
+		this.originalInput = args;
 		this.ss = args[0];
 		this.e1 = args[1];
 		this.inter = args[2];
 		this.e2 = args[3];
-		this.elunch1 = args[4];
-		this.slunch2 = args[5];
-		this.e3l1 = args[6];
-		this.e3l2 = args[7];
+		this.rally = args[4];
+		this.lunch1 = args[5];
+		this.e3 = args[6];
+		this.lunch2 = args[7];
 		this.e4 = args[8];
 	}
 }
--- src/us/rjuhsd/ohs/OHSApp/OHSPeriodClock.java
+++ src/us/rjuhsd/ohs/OHSApp/OHSPeriodClock.java
@@ -9,112 +9,41 @@ public class OHSPeriodClock {
 	private TextView t2;
 	private int h;
 	private int m;
+	private Calendar c;
 	private DailySchedualEnum day;
+	private String[] translationArray = {"Before School", "First Period", "Intervention", "Second Period", "Rally", "First Lunch", "Third Period", "Second Lunch", "Fourth Period", "After School", "", "", "", "", "", "Third Period", "", "Third Period", "", ""};
 
 	public OHSPeriodClock(TextView t1, TextView t2, DailySchedualEnum day) {
 		this.t1 = t1;
 		this.t2 = t2;
 		this.day = day;
+		c = Calendar.getInstance();
 		findPeriod();
 	}
 
+	//Doubt this'll work, it totally does.  Needs more testing throughout the day.
 	protected void findPeriod() {
-		Calendar c = Calendar.getInstance();
 		this.h = c.get(Calendar.HOUR_OF_DAY);
 		this.m = c.get(Calendar.MINUTE);
-		if(h > day.e4[0]) {
-			t1.setText("After School");
-			t2.setText("");
-		}
-		if(h <= day.e4[0]) {
-			if(h == day.e4[0]) {
-				if(m <= day.e4[1]) {
-					t1.setText("Fourth Period");
-					t2.setText("");
+		int[][] tempArray = day.originalInput;
+
+		for(int i = 0; i < tempArray.length; i++) {
+			if(h <= tempArray[i][0]) {
+				if(h == tempArray[i][0]) {
+					if(m <= tempArray[i][1]) {
+						t1.setText(translationArray[i]);
+						t2.setText(translationArray[i + 10]);
+						break;
+					}
 				} else {
-					t1.setText("After School");
-					t2.setText("");
+					t1.setText(translationArray[i]);
+					t2.setText(translationArray[i + 10]);
+					break;
 				}
-			} else {
-				t1.setText("Fourth Period");
-				t2.setText("");
 			}
-		}
-		if(h <= day.e3l1[0]) {
-			if(h == day.e3l1[0]) {
-				if(m <= day.e3l1[1]) {
-					t1.setText("Second Lunch");
-					t2.setText("Third Period");
-				}
-			} else {
-				t1.setText("Second Lunch");
-				t2.setText("Third Period");
-			}
-		}
-		if(h <= day.e3l2[0]) {
-			if(h == day.e3l2[0]) {
-				if(m <= day.e3l2[1]) {
-					t1.setText("Third Period");
-					t2.setText("");
-				}
-			} else {
-				t1.setText("Third Period");
-				t2.setText("");
-			}
-		}
-		if(h <= day.elunch1[0]) {
-			if(h == day.elunch1[0]) {
-				if(m <= day.elunch1[1]) {
-					t1.setText("First Lunch");
-					t2.setText("Third Period");
-				}
-			} else {
-				t1.setText("First Lunch");
-				t2.setText("Third Period");
-			}
-		}
-		if(h <= day.e2[0]) {
-			if(h == day.e2[0]) {
-				if(m <= day.e2[1]) {
-					t1.setText("Second Period");
-					t2.setText("");
-				}
-			} else {
-				t1.setText("Second Period");
-				t2.setText("");
-			}
-		}
-		if(h <= day.inter[0]) {
-			if(h == day.inter[0]) {
-				if(m <= day.inter[1]) {
-					t1.setText("Intervention");
-					t2.setText("");
-				}
-			} else {
-				t1.setText("Intervention");
-				t2.setText("");
-			}
-		}
-		if(h <= day.e1[0]) {
-			if(h == day.e1[0]) {
-				if(m <= day.e1[1]) {
-					t1.setText("First Period");
-					t2.setText("");
-				}
-			} else {
-				t1.setText("First Period");
-				t2.setText("");
-			}
-		}
-		if(h <= day.ss[0]) {
-			if(h == day.ss[0]) {
-				if(m <= day.ss[1]) {
-					t1.setText("Before School");
-					t2.setText("");
-				}
-			} else {
-				t1.setText("Before School");
-				t2.setText("");
+			if(i == tempArray.length-1) {
+				t1.setText(translationArray[i+1]);
+				break;
 			}
 		}
 	}
