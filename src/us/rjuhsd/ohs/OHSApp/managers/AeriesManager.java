6bf77467cebfe824d59d464610d788f4efdd4199
Jon Janzen <jjjonjanzen@gmail.com>
2014-02-03 20:57:57 -0800

    Moved the classes fetching functionality to an AsyncTask to make it UI thread independent.

--- src/us/rjuhsd/ohs/OHSApp/managers/AeriesManager.java
+++ src/us/rjuhsd/ohs/OHSApp/managers/AeriesManager.java
@@ -6,122 +6,40 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.message.BasicNameValuePair;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import android.util.Log;
 import us.rjuhsd.ohs.OHSApp.SchoolClass;
-import us.rjuhsd.ohs.OHSApp.https.HttpsClientFactory;
+import us.rjuhsd.ohs.OHSApp.tasks.GradesOverviewTask;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class AeriesManager {
 
-	private static String LOGIN_URL = "https://homelink.rjuhsd.us/LoginParent.aspx";
-	//private static String DEFAULT_URL = "http://homelink.rjuhsd.us/Default.aspx";
+	public static String LOGIN_URL = "https://homelink.rjuhsd.us/LoginParent.aspx";
+	//public static String DEFAULT_URL = "http://homelink.rjuhsd.us/Default.aspx";
+
+	Activity activity;
+
+	private boolean loadingGrades = true;
+	private GradesOverviewTask gradesTask;
 
 	private ArrayList<SchoolClass> grades;
 
-	public ArrayList<SchoolClass> getGradesOverview(Activity activity) {
-		return getGradesOverview(activity, false);
+	public void getGradesOverview(Activity activity) {
+		if(grades != null) {
+			gradesTask.inflateList(activity);
+		} else {
+			startLoadingGrades(activity);
+		}
 	}
 
-	private ArrayList<SchoolClass> getGradesOverview(final Activity activity, boolean updateFlag) {
-		if (!updateFlag && this.grades != null) { //Check if an update is necessary
-			return this.grades;
-		}
-		ArrayList<SchoolClass> grades = new ArrayList<SchoolClass>();
-		try {
-			String[] loginData = aeriesLoginData(activity);
-			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-			nvps.add(new BasicNameValuePair("portalAccountUsername", loginData[0]));
-			nvps.add(new BasicNameValuePair("portalAccountPassword", loginData[1]));
-			nvps.add(new BasicNameValuePair("checkCookiesEnabled", "true"));
-			nvps.add(new BasicNameValuePair("checkSilverlightSupport", "true"));
-			nvps.add(new BasicNameValuePair("checkMobileDevice", "false"));
-			nvps.add(new BasicNameValuePair("checkStandaloneMode", "false"));
-			nvps.add(new BasicNameValuePair("checkTabletDevice", "false"));
-
-			HttpPost request = new HttpPost(LOGIN_URL);
-			request.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
-			HttpClient client = HttpsClientFactory.sslClient();
-			HttpResponse response = client.execute(request);
-
-			Document doc = Jsoup.parse(response.getEntity().getContent(), null, LOGIN_URL);
-			int rowCount = 1;
-			while (true) {
-				String trId = "tr#ctl00_MainContent_ctl19_DataDetails_ctl0" + rowCount + "_trGBKItem";
-				Element tr = doc.select(trId).first();
-				if (tr == null) {
-					if (rowCount == 1) {
-						AlertDialog.Builder adb = new AlertDialog.Builder(activity)
-								.setTitle("Login Failure!")
-								.setMessage("Something went wrong while getting your grades...") //This should never show
-								.setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int which) {
-										activity.finish();
-									}
-								});
-						if(loginData[0].equals("") || loginData[1].equals("")) {
-							adb.setMessage("Please check that you have entered your Aeries information in preferences correctly");
-						} else {
-							adb.setMessage("Either the grades system is unavailable or your login is incorrect");
-						}
-						adb.show();
-						return grades;
-					}
-					break;
-				} else {
-					SchoolClass sClass = new SchoolClass(rowCount - 1);
-					Elements tds = tr.select("td");
-					Element className = tds.get(1);
-					if (className != null) {
-						sClass.className = className.text();
-					}
-					Element period = tds.get(2);
-					if (period != null) {
-						sClass.period = period.text();
-					}
-					Element teacherName = tds.get(3);
-					if (teacherName != null) {
-						sClass.teacherName = teacherName.text();
-					}
-					Element percentage = tds.get(4);
-					if (percentage != null) {
-						sClass.percentage = percentage.text();
-					}
-					Element mark = tds.get(6);
-					if (mark != null) {
-						sClass.mark = mark.text();
-					}
-					Element missingAssign = tds.get(8);
-					if (missingAssign != null) {
-						sClass.missingAssign = missingAssign.text();
-					}
-					Element lastUpdate = tds.get(10);
-					if (lastUpdate != null) {
-						sClass.lastUpdate = lastUpdate.text();
-					}
-					grades.add(sClass);
-				}
-				rowCount++;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		this.grades = grades;
-		return grades;
+	private void startLoadingGrades(final Activity activity) {
+		this.activity = activity;
+		loadingGrades = true;
+		gradesTask = new GradesOverviewTask(activity, this);
+		gradesTask.execute();
 	}
 
-	private String[] aeriesLoginData(Context context) {
+	public static String[] aeriesLoginData(Context context) {
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		String[] toReturn = new String[2];
 		toReturn[0] = prefs.getString("aeries_username", "");
@@ -131,5 +49,26 @@ public class AeriesManager {
 
 	public SchoolClass getById(int id) {
 		return grades.get(id);
+	}
+
+	public void setSchoolClasses(ArrayList<SchoolClass> grades) {
+		Log.d("SchoolClasses","Loaded school classes");
+		this.grades = grades;
+	}
+
+	public void setGradesLoaded(boolean isDone) {
+		this.loadingGrades = isDone;
+	}
+
+	public void errorLoadingGrades(String errorText) {
+		AlertDialog.Builder adb = new AlertDialog.Builder(activity)
+				.setTitle("Login Failure!")
+				.setMessage(errorText)
+				.setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						activity.finish();
+					}
+				});
+		adb.show();
 	}
 }
---
+++ src/us/rjuhsd/ohs/OHSApp/tasks/GradesOverviewTask.java
@@ -0,0 +1,167 @@
+package us.rjuhsd.ohs.OHSApp.tasks;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import us.rjuhsd.ohs.OHSApp.GradesArrayAdapter;
+import us.rjuhsd.ohs.OHSApp.OHSApplication;
+import us.rjuhsd.ohs.OHSApp.R;
+import us.rjuhsd.ohs.OHSApp.SchoolClass;
+import us.rjuhsd.ohs.OHSApp.activities.GradesDetailActivity;
+import us.rjuhsd.ohs.OHSApp.https.HttpsClientFactory;
+import us.rjuhsd.ohs.OHSApp.managers.AeriesManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradesOverviewTask extends AsyncTask<Void, Void, Void> {
+	Activity activity;
+	ProgressDialog progressDialog;
+	AeriesManager aeriesManager;
+	ArrayList<SchoolClass> grades;
+	String error = "An unknown error occurred while loading your classes"; //This text should never appear, its the default
+
+	public GradesOverviewTask(Activity activity, AeriesManager aeriesManager) {
+		this.activity = activity;
+		this.aeriesManager = aeriesManager;
+	}
+
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		progressDialog = new ProgressDialog(activity);
+		progressDialog.setMessage("Loading Classes. Please Wait");
+		progressDialog.setIndeterminate(false);
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		progressDialog.setCancelable(true);
+		progressDialog.show();
+	}
+
+	@Override
+	protected Void doInBackground(Void... voids) {
+		grades = new ArrayList<SchoolClass>();
+		try {
+			String[] loginData = AeriesManager.aeriesLoginData(activity);
+			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			nvps.add(new BasicNameValuePair("portalAccountUsername", loginData[0]));
+			nvps.add(new BasicNameValuePair("portalAccountPassword", loginData[1]));
+			nvps.add(new BasicNameValuePair("checkCookiesEnabled", "true"));
+			nvps.add(new BasicNameValuePair("checkSilverlightSupport", "true"));
+			nvps.add(new BasicNameValuePair("checkMobileDevice", "false"));
+			nvps.add(new BasicNameValuePair("checkStandaloneMode", "false"));
+			nvps.add(new BasicNameValuePair("checkTabletDevice", "false"));
+
+			HttpPost request = new HttpPost(AeriesManager.LOGIN_URL);
+			request.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
+			HttpClient client = HttpsClientFactory.sslClient();
+			HttpResponse response = client.execute(request);
+
+			Document doc = Jsoup.parse(response.getEntity().getContent(), null, AeriesManager.LOGIN_URL);
+			int rowCount = 1;
+			while (true) {
+				String trId = "tr#ctl00_MainContent_ctl19_DataDetails_ctl0" + rowCount + "_trGBKItem";
+				Element tr = doc.select(trId).first();
+				if (tr == null) {
+					if (rowCount == 1) {
+						//No data was loaded, calling it quits here and posting a dialog explaining why
+						if(loginData[0].equals("") || loginData[1].equals("")) {
+							error = "Please check that you have entered your Aeries information in preferences correctly";
+						} else {
+							error = "Either the grades system is unavailable or your login is incorrect";
+						}
+						cancel(true);
+						return null;
+					}
+					break;
+				} else {
+					SchoolClass sClass = new SchoolClass(rowCount - 1);
+					Elements tds = tr.select("td");
+					Element className = tds.get(1);
+					if (className != null) {
+						sClass.className = className.text();
+					}
+					Element period = tds.get(2);
+					if (period != null) {
+						sClass.period = period.text();
+					}
+					Element teacherName = tds.get(3);
+					if (teacherName != null) {
+						sClass.teacherName = teacherName.text();
+					}
+					Element percentage = tds.get(4);
+					if (percentage != null) {
+						sClass.percentage = percentage.text();
+					}
+					Element mark = tds.get(6);
+					if (mark != null) {
+						sClass.mark = mark.text();
+					}
+					Element missingAssign = tds.get(8);
+					if (missingAssign != null) {
+						sClass.missingAssign = missingAssign.text();
+					}
+					Element lastUpdate = tds.get(10);
+					if (lastUpdate != null) {
+						sClass.lastUpdate = lastUpdate.text();
+					}
+					grades.add(sClass);
+				}
+				rowCount++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		((OHSApplication)activity.getApplication()).aeriesManager.setSchoolClasses(grades);
+		((OHSApplication)activity.getApplication()).aeriesManager.setGradesLoaded(false);
+		return null;
+	}
+
+	@Override
+	protected void onCancelled() {
+		super.onCancelled();
+		aeriesManager.errorLoadingGrades(error);
+	}
+
+	@Override
+	protected void onPostExecute(Void v) {
+		super.onPostExecute(v);
+		if(isCancelled()) {
+			onCancelled();
+			return;
+		}
+		aeriesManager.setSchoolClasses(grades);
+		inflateList(activity);
+		progressDialog.dismiss();
+	}
+
+	public void inflateList(final Activity act) {
+		final ArrayAdapter adapter = new GradesArrayAdapter(activity, R.layout.grades_list_item, grades);
+		final ListView listview = (ListView) act.findViewById(R.id.listview);
+		listview.setAdapter(adapter);
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Intent gradeDetailIntent = new Intent(act, GradesDetailActivity.class);
+				gradeDetailIntent.putExtra("schoolClassId",arg2);
+				act.startActivity(gradeDetailIntent);
+			}
+
+		});
+	}
+}
