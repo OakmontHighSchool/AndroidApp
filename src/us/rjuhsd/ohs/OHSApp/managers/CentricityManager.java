57fae1ce181735866c731e3be9c8a0293895e912
Jon Janzen <jjjonjanzen@gmail.com>
2014-02-04 09:50:54 -0800

    Fixed class name and usages

--- src/us/rjuhsd/ohs/OHSApp/managers/CentricityManager.java
+++ src/us/rjuhsd/ohs/OHSApp/managers/CentricityManager.java
@@ -14,7 +14,7 @@ import us.rjuhsd.ohs.OHSApp.R;
 
 import java.io.IOException;
 
-public class CentricityManager extends AsyncTask<Void, articleWrapper, Void>{
+public class CentricityManager extends AsyncTask<Void, ArticleWrapper, Void>{
 	private static String URL = "http://ohs.rjuhsd.us/site/default.aspx?PageID=1";
 	private static int ID = 20;
 	private static Document doc;
@@ -29,23 +29,23 @@ public class CentricityManager extends AsyncTask<Void, articleWrapper, Void>{
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		publishProgress(new articleWrapper(doc.select("div.ui-article").get(ID)));
+		publishProgress(new ArticleWrapper(doc.select("div.ui-article").get(ID)));
 		return null;
 	}
 
 	@Override
-	public void onProgressUpdate(articleWrapper... item) {
-		articleWrapper aw = item[0];
+	public void onProgressUpdate(ArticleWrapper... item) {
+		ArticleWrapper aw = item[0];
 		OHSNotificationHandler.addNotification(R.drawable.test, aw.articleHeader.text(), aw.articleText.text());
 	}
 }
 
-class articleWrapper {
+class ArticleWrapper {
 	public Element articleSource;
 	public Elements articleHeader;
 	public Elements articleText;
 
-	public articleWrapper(Element el) {
+	public ArticleWrapper(Element el) {
 		articleSource = el;
 		articleHeader = el.select("h1.ui-article-title").select("span");
 		articleText = el.select("p.ui-article-description");
