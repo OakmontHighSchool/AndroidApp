7cf72964266d6b9c7d053c2be9865baab50204c7
Jon Janzen <jjjonjanzen@gmail.com>
2014-02-06 10:52:58 -0800

    Fixed compile errors and added a checkbox preference for autologins

--- src/us/rjuhsd/ohs/OHSApp/WiFiStateReceiver.java
+++ src/us/rjuhsd/ohs/OHSApp/WiFiStateReceiver.java
@@ -18,7 +18,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.message.BasicNameValuePair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import us.rjuhsd.ohs.OHSApp.https.HttpsClientFactory;
 
 import java.io.IOException;
@@ -28,14 +27,17 @@ import java.util.List;
 public class WiFiStateReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		if(prefs.getBoolean("school_wifi_login_flag",false)) return;
 		NetworkInfo ni = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO); //We are the knights who say NI!
 		Log.d("WIFILOGIN","Starting autologin");
+		if(ni == null) return;
 		if(ni.isConnected()) {
 			final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 			final WifiInfo connectionInfo = wifiManager.getConnectionInfo();
 			if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.getSSID())) {
 				String ssid = connectionInfo.getSSID();
-				if(ssid.toLowerCase().equals("ohs-guest")) { //TODO: Check this, may not be correct
+				if(ssid.toLowerCase().equals("ohs-guest")) {
 					try {
 						Log.d("WIFILOGIN","Trying login");
 						final String LOGIN_URL = "http://172.31.0.2/cgi-bin/nph-xauth";
@@ -45,15 +47,15 @@ public class WiFiStateReceiver extends BroadcastReceiver {
 						if(k1 == null) k1 = "";
 						String k2 = doc.select("input[name=\"K2\"]").first().text();
 						if(k2 == null) k2 = "";
-						Element url = doc.select("input[name=\"URL\"]").first();
-						SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+						String url = doc.select("input[name=\"URL\"]").first().text();
+						if(url == null) url = "";
 						String username = prefs.getString("school_username", "");
 						String password = prefs.getString("school_password","");
 
 						List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-						nvps.add(new BasicNameValuePair("K1", k1.text()));
-						nvps.add(new BasicNameValuePair("K2", k2.text()));
-						nvps.add(new BasicNameValuePair("URL", url.text()));
+						nvps.add(new BasicNameValuePair("K1", k1));
+						nvps.add(new BasicNameValuePair("K2", k2));
+						nvps.add(new BasicNameValuePair("URL", url));
 						nvps.add(new BasicNameValuePair("user", username));
 						nvps.add(new BasicNameValuePair("pass", password));
 
