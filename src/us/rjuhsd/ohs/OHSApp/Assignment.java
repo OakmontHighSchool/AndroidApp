cda105303890b6e0c5bcc4d95c4773dcd9d8a6b4
Jon Janzen <jjjonjanzen@gmail.com>
2014-02-10 22:13:01 -0800

    Adding GradeDetailTask to retrieve assignments and stuff, not done

---
+++ src/us/rjuhsd/ohs/OHSApp/Assignment.java
@@ -0,0 +1,14 @@
+package us.rjuhsd.ohs.OHSApp;
+
+public class Assignment {
+	String description;
+	String Type;
+	String category;
+	String score;
+	String correct;
+	String precent;
+	String status;
+	String dateCompleted;
+	String dateDue;
+	boolean gradingComplete;
+}
