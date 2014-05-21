8846d0462b438b9905b3a1fbd4d8eb02844e9b1a
Jon Janzen <jjjonjanzen@gmail.com>
2014-05-20 21:21:37 -0700

    Cleanup NewsActivity

--- src/us/rjuhsd/ohs/OHSApp/activities/NewsActivity.java
+++ src/us/rjuhsd/ohs/OHSApp/activities/NewsActivity.java
@@ -4,9 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import us.rjuhsd.ohs.OHSApp.R;
 import us.rjuhsd.ohs.OHSApp.Tools;
 import us.rjuhsd.ohs.OHSApp.drawer.DrawerList;
@@ -20,8 +18,6 @@ import java.util.List;
 
 public class NewsActivity extends Activity {
 	//Variables:
-	private TextView StaticText2;
-	private LinearLayout linearLayout;
 	private DrawerLayout drawerLayout;
 	private ListView drawerList;
 	public final List<ArticleWrapper> articleWrapperList = new ArrayList<ArticleWrapper>();
@@ -60,9 +56,6 @@ public class NewsActivity extends Activity {
 	}
 
 	void getAllByID() {
-		StaticText2 = (TextView) this.findViewById(R.id.news_title);
-		linearLayout = (LinearLayout) this.findViewById(R.id.news_linear_layout);
-
 		drawerLayout = (DrawerLayout) this.findViewById(R.id.news_drawer_layout);
 		drawerList = (ListView) this.findViewById(R.id.news_drawer_list);
 	}
